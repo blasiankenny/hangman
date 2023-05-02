b05ded5280244a60e8094d3e4414d0b19fbd3a79
Kenny <88187389+blasiankenny@users.noreply.github.com>
2023-05-02 13:13:04 -0700

    Add files via upload

---
+++ python/dokusyu/selfpy/chap08/practice/avg.py
@@ -0,0 +1,4 @@
+def average(*values):
+    return sum(values)/len(values)
+
+print(average(1,2,3,4))
---
+++ python/dokusyu/selfpy/chap08/practice/practice.py
@@ -0,0 +1,5 @@
+def get_square(base=1,height=1):
+    return base*height
+
+
+print(get_square(10,3))
